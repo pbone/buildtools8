89262d4c88bee5ac1b982efcebb41c79b0d53abb
Patrick Hall <patrickhall@patrickimac2.ibi.com>
2017-05-02 14:56:07 -0400

    BLT-1: first commit of buildtools8.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,18 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['buildtools8.local'] = array(
+  'root' => '/var/www/buildtools8/docroot',
+  'uri' => 'http://local.buildtools8.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['buildtools8.local'] += array(
+    'remote-host' => 'local.buildtools8.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
